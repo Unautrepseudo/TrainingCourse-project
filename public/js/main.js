ee82a9f8b7db0bd0d0ce2a35ce51d1e5639435d7
Eric <eric.narbo@gmail.com>
2020-01-12 02:27:41 +0100

    Home Js scroll effect

--- public/js/main.js
+++ public/js/main.js
@@ -67,4 +67,31 @@ document.getElementById('infoImg3').addEventListener("mouseover", (() =>{
             $('#face').attr('src','img/faceb.png');
     }));
      
-        
+ 
+
+/***** Effet scroll whole page (HOME) ********/
+const threshold = .2
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold
+}
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > threshold) {
+      entry.target.classList.remove('reveal')
+      observer.unobserve(entry.target)
+    }
+  })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+
+window.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver(handleIntersect, options)
+  const targets = document.querySelectorAll('.reveal')
+  targets.forEach(function (target) {
+    observer.observe(target)
+  })
+})
