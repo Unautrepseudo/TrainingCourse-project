5cc7b847651e6d35c8723e251a902df6b6bbf58d
AnemoneK <kubani2001@yahoo.fr>
2020-01-17 17:34:16 +0100

    page des artisans manque le visuel

--- src/Controller/ArtisansController.php
+++ src/Controller/ArtisansController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Artisan;
+use App\Form\ArtisanType;
+use App\Repository\ArtisanRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArtisansController extends AbstractController
@@ -10,10 +15,11 @@ class ArtisansController extends AbstractController
     /**
      * @Route("/artisans", name="artisans")
      */
-    public function index()
+    public function index(ArtisanRepository $artisanRepository): Response
     {
         return $this->render('artisans/index.html.twig', [
-            'controller_name' => 'ArtisansController',
+            'artisans' => $artisanRepository->findAll(),
         ]);
     }
 }
+
