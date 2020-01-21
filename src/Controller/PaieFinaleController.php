40e6265000f73375b3c5001a81787032739ff43f
AnemoneK <kubani2001@yahoo.fr>
2020-01-21 11:46:57 +0100

    paie page

--- src/Controller/PaieFinaleController.php
+++ src/Controller/PaieFinaleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PaieFinaleController extends AbstractController
@@ -10,10 +12,36 @@ class PaieFinaleController extends AbstractController
     /**
      * @Route("/paie/finale", name="paie_finale")
      */
-    public function index()
+    public function index(Session $session, ProductRepository $ProductRepository)
     {
+        $panier = $session->get('panier', []);
+
+        $panierData = [];
+
+        foreach($panier as $id => $quantity){
+            $panierData[] = [
+                'product' => $ProductRepository->find($id),
+                'quantity'=> $quantity
+            ];
+
+          
+        }
+
+       
+//---------------------------------------------------------------------------------------------
+       $total = 0;
+
+       foreach($panierData as $item )
+       {
+           $totalitem = $item['product']->getPrice() * $item['quantity'];
+           $total += $totalitem;
+
+       }
+
         return $this->render('paie_finale/index.html.twig', [
             'controller_name' => 'PaieFinaleController',
+            'items'=>$panierData,
+            'total'=>$total 
         ]);
     }
 }
